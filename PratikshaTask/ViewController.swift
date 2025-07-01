701c7212f161b70b4785609f95da98e266bc51d6
PratikshaMohadare <pratiksha.mohadare9@gmail.com>
2025-07-01 23:05:45 +0530

    Initial Commit

---
+++ PratikshaTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PratikshaTask
+//
+//  Created by Pratiksha on 01/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
